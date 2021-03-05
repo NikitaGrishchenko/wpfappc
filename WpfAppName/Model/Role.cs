b6341147e4522639ded4efca6622c68e64b7fba2
Nikita <grishchenkonikita@mail.ru>
2021-03-05 23:10:34 +0300

    lab 1

---
+++ WpfAppName/Model/Role.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WpfAppName.Model
+{
+    public class Role
+    {
+        public int Id { get; set; }
+        public string NameRole { get; set; }
+        public Role() { }
+        public Role(int id, string nameRole)
+        {
+            this.Id = id;
+            this.NameRole = nameRole;
+        }
+    }
+
+}
